b1a596fbe55fd47d5da62749407460a6adb73a11
a01740729 <a01740729@itesm.mx>
2021-08-20 01:22:41 +0000

    area y volumen esfera

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,14 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+    r = float(input('Dame el valor del radio: '))
+
+    a = (4 * math.pi * r ** 2)
+    v = ((4 * math.pi * r ** 3) / 3)
+
+    print('Area: '+str(a))
+    print('Volumen: '+str(v))
 
 if __name__=='__main__':
     main()
